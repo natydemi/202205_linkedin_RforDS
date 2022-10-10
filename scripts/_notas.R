0ebf06097bbf12a0c8d00c7edfc47463c57912ec
Nath <natydemi@gmail.com>
2022-10-10 14:29:29 -0300

    add funcoes basicas no mod 03

--- scripts/_notas.R
+++ scripts/_notas.R
@@ -216,3 +216,76 @@ state
     #(português)
     # Curso-R ~ material em português
     # https://livro.curso-r.com/
+
+
+# poket ------
+
+#para instalar uma biblioteca
+install.packages("tidyverse") 
+
+#para utilizar as funções de uma biblioteca instalada, é necessário carrega-la
+#dando um tick na seção 'Packages' ou
+library(tidyverse)
+
+
+#Todas as bibliotecas abaixo são instaladas junto com o tidyverse:
+#mas apenas o 1º grupo é carregado, as demais precisam ser carregadas individualmente
+
+#instaladas e carregadas 
+# ggplot2, for data visualisation.
+# dplyr, for data manipulation.
+# tidyr, for data tidying.
+# readr, for data import.
+# purrr, for functional programming.
+# tibble, for tibbles, a modern re-imagining of data frames.
+
+#instaladas mas não carregadas 
+# Importing other types of data:
+# readxl, for .xls and .xlsx files.
+# feather, for sharing with Python and other languages.
+# haven, for SPSS, SAS and Stata files.
+# httr, for web apis.
+# jsonlite for JSON.
+# rvest, for web scraping.
+# xml2, for XML.
+# Working with specific types of data:
+# hms, for times.
+# stringr, for strings.
+# lubridate, for date/times.
+# forcats, for factors.
+# Modelling
+# modelr, for modelling within a pipeline
+# broom, for turning models into tidy data
+
+
+
+#vamos refazer o exercicio de salvar uma base do R como .csv
+
+#escrevendo
+#antigo
+write.csv(mtcars, file = "aulaR_dados.csv")
+#novo
+write_csv(mtcars, path = "aulaR_dados.csv")
+#vantagens do novo: mais rápido, tem mais opções, e
+#tem o msm padrão de sintaxe de várias outras funções do tidyverse  
+
+#lendo
+#antigo
+read.csv("aulaR_dados.csv") 
+#novo
+read_csv("aulaR_dados.csv") 
+#vantagens do novo: além das citadas no write_csv, note que o arquivo 
+#lido já está no formato tidy, ou seja, mais organizado   
+
+#excel
+#escrevendo 
+#eu costumo utilizar a biblioteca openxlsx para escrever excel
+install.packages("openxlsx")
+library(openxlsx)
+write.xlsx(mtcars, file = "aulaR_dados.xlsx")
+#lendo    
+#costumo usar a biblioteca readxl, do tidyverse
+library(readxl)
+read_excel("aulaR_dados.xlsx")
+
+#lembrando que para ver + opções de parâmetros das funções é só consultar o help
