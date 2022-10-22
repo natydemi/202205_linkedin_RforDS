6490e14cf1a0a73bbf5d1278cb376f03d58e2feb
Nathalia Demetrio + Linkedin <natydemi@gmail.com>
2022-10-22 14:38:08 +0200

    terminei as gravaçõesgit add -Agit add -A!

---
+++ scripts/mod00_bibliotecas_utilizadas_no_curso.R
@@ -0,0 +1,19 @@
+
+#Você irá precisar no Instalação R, Rstudio, e os pacotes:  
+  
+pacotes_necessarios <- c(" tidyverse ",
+                         
+                         " praise ",
+                         
+                         " devtools ",
+                         
+                         " data.table ",
+                         
+                         "rmarkdown",
+                         
+                         "dados",)
+
+install.packages(pacotes_necessarios) 
+  
+  
+  
